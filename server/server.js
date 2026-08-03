f4d3fa6b73e1611f8235b96759807cc9245cfcb2
Sai Pranav <saipranavviswanatha2006@gmail.com>
2026-08-03 22:47:59 +0530

    day 4

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const { PrismaClient } = require("./generated/prisma");
 
 const app = express();
+const prisma = new PrismaClient();
 const PORT = 5000;
 
 app.use(cors());
 
-app.get("/api/health", (req, res) => {
-    res.json({
-        status: "ok",
-        message: "Level Up API is running"
-    });
+app.get("/api/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+
+    res.json(users);
 });
 
 app.listen(PORT, () => {
