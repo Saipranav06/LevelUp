f7c068d1fde37c7280c09ca49ab977a498de49cf
Sai Pranav <saipranavviswanatha2006@gmail.com>
2026-08-05 17:17:22 +0530

    day 5

--- server/server.js
+++ server/server.js
@@ -1,17 +1,50 @@
+require("dotenv").config();
+
+
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require("./generated/prisma");
+
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+    adapter,
+});
 
 const app = express();
-const prisma = new PrismaClient();
 const PORT = 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/users", async (req, res) => {
     const users = await prisma.user.findMany();
 
     res.json(users);
+});
+
+app.post("/api/register", async (req, res) => {
+
+    const { username, email, password, role } = req.body;
+
+    const user = await prisma.user.create({
+    data: {
+        username,
+        email,
+        password,
+        role
+    }
+});
+
+res.json(user);
+
 });
 
 app.listen(PORT, () => {
